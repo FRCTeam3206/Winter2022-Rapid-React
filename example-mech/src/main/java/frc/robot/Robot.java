c4bd53d30e745d135cd6cdcd8c7920f55e63253b
StrangestMan <strangestalt@protonmail.com>
2022-01-22 13:44:30 -0600

    Add mech code

---
+++ example-mech/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+//import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+//import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+//import edu.wpi.first.wpilibj.motorcontrol.Victor;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+
+/** This is a demo program showing how to use Mecanum control with the MecanumDrive class. */
+public class Robot extends TimedRobot {
+  private static final int kFrontLeftChannel = 0;
+  private static final int kRearLeftChannel = 2;
+  private static final int kFrontRightChannel = 1;
+  private static final int kRearRightChannel = 3;
+
+  private static final int kXBOXChannel = 0;
+
+  private MecanumDrive m_robotDrive;
+
+  private XboxController m_stick;
+
+  @Override
+  public void robotInit() {
+    // 8 controllers for 8 motors
+    VictorSP frontLeft = new VictorSP(kFrontLeftChannel);    // Front Left
+    VictorSP rearLeft = new VictorSP(kRearLeftChannel);       // Back Left
+    VictorSP frontRight = new VictorSP(kFrontRightChannel);  // Front Right
+    VictorSP rearRight = new VictorSP(kRearRightChannel);     // Back Right
+
+    // Invert the right side motors.
+    // You may need to change or remove this to match your robot.
+    //frontRight.setInverted(true);
+    //rearRight.setInverted(true);
+
+    m_robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
+
+    m_stick = new XboxController(kXBOXChannel);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // Use the joystick X axis for lateral movement, Y axis for forward
+    // movement, and Z axis for rotation.
+    m_robotDrive.driveCartesian(m_stick.getRightY(), m_stick.getRightX(), m_stick.getLeftX(), 0.0);
+  }
+}
