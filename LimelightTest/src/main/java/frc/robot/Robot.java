295983d1845d0b079f43312ce991bc66d91d4b55
CREATE_01 <frc-create@outlook.com>
2022-01-22 10:19:18 -0600

    Added capacity to turn based on limelight input

--- LimelightTest/src/main/java/frc/robot/Robot.java
+++ LimelightTest/src/main/java/frc/robot/Robot.java
@@ -1,86 +1,94 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.cameraserver.CameraServer;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the manifest file in the resource
- * directory.
+ * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
+ * the code necessary to operate a robot with tank drive.
  */
-public class Robot extends TimedRobot {
-  private final PWMSparkMax m_leftDrive = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightDrive = new PWMSparkMax(1);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive, m_rightDrive);
-  private final Joystick m_stick = new Joystick(0);
-  private final Timer m_timer = new Timer();
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+public class Robot extends TimedRobot {
+  private DifferentialDrive m_myRobot;
+  private XboxController m_joystick;
+  private static final int leftLeadDeviceID = 1; 
+  private static final int rightLeadDeviceID = 3;
+  private static final int leftFollowDeviceID = 2;
+  private static final int rightFollowDeviceID = 4;
+  private CANSparkMax m_leftLeadMotor;
+  private CANSparkMax m_rightLeadMotor;
+  private CANSparkMax m_leftFollowMotor;
+  private CANSparkMax m_rightFollowMotor;
+  private double lastVelocity=0;
+  private long lastTime;
+
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightDrive.setInverted(true);
+    m_leftLeadMotor = new CANSparkMax(leftLeadDeviceID, MotorType.kBrushed);
+    m_leftFollowMotor = new CANSparkMax(leftFollowDeviceID, MotorType.kBrushed);
+    m_rightLeadMotor = new CANSparkMax(rightLeadDeviceID, MotorType.kBrushed);
+    m_rightFollowMotor = new CANSparkMax(rightFollowDeviceID, MotorType.kBrushed);
+
+    m_leftFollowMotor.follow(m_leftLeadMotor);
+    m_rightFollowMotor.follow(m_rightLeadMotor);
+
+    m_leftLeadMotor.setInverted(true);
+
+    m_myRobot = new DifferentialDrive(m_leftLeadMotor, m_rightLeadMotor);
+    m_joystick = new XboxController(0);
+
+    CameraServer.startAutomaticCapture();
+
+    lastTime=System.currentTimeMillis();
   }
 
-  /** This function is run once each time the robot enters autonomous mode. */
-  @Override
-  public void autonomousInit() {
-    m_timer.reset();
-    m_timer.start();
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-    
-  }
-
-  /** This function is called once each time the robot enters teleoperated mode. */
-  @Override
-  public void teleopInit() {}
-
-  /** This function is called periodically during teleoperated mode. */
-  long lastPrint=0;
+  double currRight=0;
+  double currLeft=0;
+  
   @Override
   public void teleopPeriodic() {
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    double ta=table.getEntry("tv").getDouble(0.0);
-    double tx=table.getEntry("tx").getDouble(0.0);
-    if(lastPrint+1000<=System.currentTimeMillis()){
-      if(ta>.75){
-          if(tx>10){
-            System.out.println("Right");
-          }else if(tx<10){
-            System.out.println("Left");
-          }else{
-            System.out.println("Centered");
-          }
-      }
-      lastPrint=System.currentTimeMillis();
-    }
+    
   }
-
-  /** This function is called once each time the robot enters test mode. */
   @Override
-  public void testInit() {}
+  public void autonomousPeriodic() {
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    double ta=table.getEntry("ta").getDouble(0.0);//If limelight sees a target
+    double tx=table.getEntry("tx").getDouble(0.0);
+    int direction=0;
+    if(ta>.75){
+        if(tx>5){
+          direction=1;
+        }else if(tx<5){
+          direction=-1;
+        }
+    }
+    System.out.println(ta+" "+tx+" "+direction);
+    switch(direction){
+      case 1:
+        //m_myRobot.arcadeDrive(0, .25);
+      break;
+      case -1:
+        //m_myRobot.arcadeDrive(0,-.25);
+      default:
+        //m_myRobot.arcadeDrive(0, 0);
+    }
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+  }
+  private double cut(double val){
+    if(val<-1)return -1;
+    if(val>1)return 1;
+    return val;
+  }
 }
