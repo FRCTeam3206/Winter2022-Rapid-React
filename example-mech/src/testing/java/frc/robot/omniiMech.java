7a9e217574f728c38952845640a95335d2f2788f
StrangestMan <strangestalt@protonmail.com>
2022-01-24 13:44:50 -0600

    Work towards calculating stick angle

--- example-mech/src/testing/java/frc/robot/omniiMech.java
+++ example-mech/src/testing/java/frc/robot/omniiMech.java
@@ -5,6 +5,8 @@
 package frc.robot;
 
 import java.lang.System;
+import java.util.*;       // This is temporary, I'll replace it with what exactly is used later
+
 //import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
@@ -31,22 +33,30 @@ public class Robot extends TimedRobot {
   private double xbox_xAxisLStick = 0;
   private double xbox_yAxisLStick = 0;
 
+  private double xbox_xAxisRStickMOD = 0;
+  private double xbox_yAxisRStickMOD = 0;
+  private double xbox_xAxisLStickMOD = 0;
+  private double xbox_yAxisLStickMOD = 0;
+
   private static double xbox_xAxisRStickMAX = 0; // TODO; Fill these in
   private static double xbox_yAxisRStickMAX = 0;  //-
+  
   private static double xbox_xAxisLStickMAX = 0;  //-
   private static double xbox_yAxisLStickMAX = 0;  //-
+
   private static double xbox_xAxisRStickMIN = 0;  //-
   private static double xbox_yAxisRStickMIN = 0;  //-
+  
   private static double xbox_xAxisLStickMIN = 0;  //-
   private static double xbox_yAxisLStickMIN = 0;  //-
 
-  private double xbox_rStickAngle;
-  private double xbox_lStickAngle;
+  private double xbox_rStickAngle = 0;  // These will be measured in degrees
+  private double xbox_lStickAngle = 0;
 
-  private int xbox_lStickNormalX;
-  private int xbox_lStickNormalY;
-  private int xbox_rStickNormalX;
-  private int xbox_rStickNormalY;
+  private double xbox_lStickNormalX = 0;
+  private double xbox_lStickNormalY = 0;
+  private double xbox_rStickNormalX = 0;
+  private double xbox_rStickNormalY = 0;
 
   private boolean debugMode = true;
 
@@ -75,18 +85,56 @@ public class Robot extends TimedRobot {
     return 2 * (i - min) / (max - min) - 1;
   }
 
+  public double findMinOrMax(double a, double b, boolean minOrMax) { // Find the min/max of a value. True = find max, False = find min
+    switch(minOrMax) {
+      case(true):
+          if(a > b) {
+            return a;
+          } else {
+            return b;
+          }
+        break;
+      case(false):
+          if(a < b) {
+            return a;
+          } else {
+            return b;
+          }
+        break;
+      default:
+        System.out.println("Please use a valid min/max option. True = max, false = min");
+        break;
+    }
+  }
+
   @Override
   public void teleopPeriodic() {
     // Use the joystick X axis for lateral movement, Y axis for forward
     // movement, and Z axis for rotation.
-    xbox_xAxisRStick = m_stick.getRightX() * 0.1;   // Get stick axies and "normalize"
-    xbox_yAxisRStick = m_stick.getRightY() * 0.1;     //-
-    xbox_xAxisLStick = m_stick.getLeftX() * 0.1;      //-
-    xbox_yAxisLStick = m_stick.getLeftY() * 0.1;      //-
+    xbox_xAxisRStick = m_stick.getRightX();   // Get stick axies
+    xbox_yAxisRStick = m_stick.getRightY();     //-
+    xbox_xAxisLStick = m_stick.getLeftX();      //-
+    xbox_yAxisLStick = m_stick.getLeftY();      //-
 
-    //m_robotDrive.driveCartesian(xbox_xAxisLStick, xbox_yAxisLStick, xbox_xAxisRStick, 0.0);
+    // Get max and min for each stick
+    xbox_xAxisRStickMAX = findMinOrMax(xbox_xAxisRStickMAX, xbox_xAxisRStick, true);
+    xbox_xAxisRStickMIN = findMinOrMax(xbox_xAxisRStickMIN, xbox_xAxisRStick, false);
+    xbox_yAxisRStickMAX = findMinOrMax(xbox_yAxisRStickMAX, xbox_yAxisRStick, true);
+    xbox_yAxisRStickMIN = findMinOrMax(xbox_yAxisRStickMIN, xbox_yAxisRStick, false);
+
+    xbox_xAxisLStickMAX = findMinOrMax(xbox_xAxisLStickMAX, xbox_xAxisLStick, true);
+    xbox_xAxisLStickMIN = findMinOrMax(xbox_xAxisLStickMIN, xbox_xAxisLStick, false);
+    xbox_yAxisLStickMAX = findMinOrMax(xbox_yAxisLStickMAX, xbox_yAxisLStick, true);
+    xbox_yAxisLStickMIN = findMinOrMax(xbox_yAxisLStickMIN, xbox_yAxisLStick, false);
+
+    // Weigh values down
+    xbox_xAxisRStickMOD = xbox_xAxisRStick * 0.1;
+    xbox_yAxisRStickMOD = xbox_yAxisRStick * 0.1;
+    xbox_xAxisLStickMOD = xbox_xAxisLStick * 0.1;
+    xbox_yAxisLStickMOD = xbox_yAxisLStick * 0.1;
     
-
+    //m_robotDrive.driveCartesian(xbox_xAxisLStickMOD, xbox_yAxisLStickMOD, xbox_xAxisRStickMOD, 0.0);
+    
     // Normalize stick values between -1 & 1
     xbox_rStickNormalX = normalizeStickVals(xbox_xAxisRStick, xbox_xAxisRStickMAX, xbox_xAxisRStickMIN);
     xbox_rStickNormalY = normalizeStickVals(xbox_yAxisRStick, xbox_yAxisRStickMAX, xbox_yAxisRStickMIN);
@@ -109,19 +157,32 @@ public class Robot extends TimedRobot {
         // There's more but I'm lazy and tired to describe them rn
 
     if(debugMode == true) {
-      System.out.println("Right Stick X: " + m_stick.getRightX());
-      System.out.println("Right Stick Y: " + m_stick.getRightY());
+      System.out.println("Right Stick X: " + xbox_xAxisRStick);
+      System.out.println("Right Stick Y: " + xbox_yAxisRStick);
       System.out.println("---");
-      System.out.println("Left Stick X: " + m_stick.getLeftX());
-      System.out.println("Left Stick Y: " + m_stick.getLeftY());
+      System.out.println("Left Stick X: " + xbox_xAxisLStick);
+      System.out.println("Left Stick Y: " + xbox_yAxisLStick);
 
       System.out.println("=========");
 
-      System.out.println("Right Stick X (mod): " + xbox_xAxisRStick);
-      System.out.println("Right Stick Y (mod): " + xbox_yAxisRStick);
+      System.out.println("Right Stick X (mod): " + xbox_xAxisRStickMOD);
+      System.out.println("Right Stick Y (mod): " + xbox_yAxisRStickMOD);
       System.out.println("---");
-      System.out.println("Left Stick X (mod): " + xbox_xAxisLStick);
-      System.out.println("Left Stick Y (mod): " + xbox_yAxisLStick);
+      System.out.println("Left Stick X (mod): " + xbox_xAxisLStickMOD);
+      System.out.println("Left Stick Y (mod): " + xbox_yAxisLStickMOD);
+
+      System.out.println("|||||||||");
+
+      System.out.println("Right Stick X (normal): " + xbox_rStickNormalX);
+      System.out.println("Right Stick Y (normal): " + xbox_rStickNormalY);
+      System.out.println("---");
+      System.out.println("Left Stick X (normal): " + xbox_lStickNormalX);
+      System.out.println("Left Stick Y (normal): " + xbox_lStickNormalY);
+
+      System.out.println("/////////");
+
+      System.out.println("Right Stick Angle: " + xbox_rStickAngle);
+      System.out.println("Left Stick Angle: " + xbox_lStickAngle);
     }
   }
 }
