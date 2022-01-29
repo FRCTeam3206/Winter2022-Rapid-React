03241ade2d2eb471ca1b7e2e9a5d5335cffa14ea
Connor <56924624+IrishConman@users.noreply.github.com>
2022-01-29 14:51:53 -0600

    Add several days of coding changes
    Remove unused/bad code
    Add acceleration
    Add fine tuning

--- example-mech/src/main/java/frc/robot/omnimech.java
+++ example-mech/src/main/java/frc/robot/omnimech.java
@@ -4,9 +4,6 @@
 
 package frc.robot;
 
-import java.lang.System;
-// import java.util.*;       // This is temporary, I'll replace it with what exactly is used later
-
 //import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
@@ -28,56 +25,38 @@ public class omnimech extends TimedRobot {
 
   private XboxController m_stick;
 
-  private double xbox_xAxisRStick = 0;
-  private double xbox_yAxisRStick = 0;
-  private double xbox_xAxisLStick = 0;
-  private double xbox_yAxisLStick = 0;
-
-  private double xbox_xAxisRStickMOD = 0;
-  private double xbox_yAxisRStickMOD = 0;
-  private double xbox_xAxisLStickMOD = 0;
-  private double xbox_yAxisLStickMOD = 0;
-
-  private static double xbox_xAxisRStickMAX = 0; // TODO; Fill these in
-  private static double xbox_yAxisRStickMAX = 0;  //-
-  
-  private static double xbox_xAxisLStickMAX = 0;  //-
-  private static double xbox_yAxisLStickMAX = 0;  //-
-
-  private static double xbox_xAxisRStickMIN = 0;  //-
-  private static double xbox_yAxisRStickMIN = 0;  //-
-  
-  private static double xbox_xAxisLStickMIN = 0;  //-
-  private static double xbox_yAxisLStickMIN = 0;  //-
-
-  private double xbox_rStickAngle = 0;  // These will be measured in degrees
-  private double xbox_lStickAngle = 0;
-
-  private double xbox_lStickNormalX = 0;
-  private double xbox_lStickNormalY = 0;
-  private double xbox_rStickNormalX = 0;
-  private double xbox_rStickNormalY = 0;
-
-  private boolean debugMode = true;
-
   private VictorSP frontLeft = new VictorSP(kFrontLeftChannel);    // Front Left
   private VictorSP rearLeft = new VictorSP(kRearLeftChannel);       // Back Left
   private VictorSP frontRight = new VictorSP(kFrontRightChannel);  // Front Right
   private VictorSP rearRight = new VictorSP(kRearRightChannel);     // Back Right
 
+  private double accelValues[];
+  private static int FORWARD = 0;
+  private static int STRAFE = 1;
+  private static int ROTATE = 2;
+
+  private static int ACCEL_CO = 8; // Accelleration coefficient
+  private static double STICK_MOD = 0.5; // Artifical limit on input range
+  private static double STICK_DEADZONE = 0.1; // xbox controller deadband
+
+  private double stickTotal = 0;  // Total values of all stick inputs outside the deadband
+
+  private int fakeBool_Forward = 0;
+  private int fakeBool_Strafe = 0;
+  private int fakeBool_Rotate = 0;
+
+  private double leftX_Fine = 0;
+  private double leftY_Fine = 0;
+  private double rightX_Fine = 0;
+
+  private double powVals[];
+
   @Override
   public void robotInit() {
-    /*
-    VictorSP frontLeft = new VictorSP(kFrontLeftChannel);    // Front Left
-    VictorSP rearLeft = new VictorSP(kRearLeftChannel);       // Back Left
-    VictorSP frontRight = new VictorSP(kFrontRightChannel);  // Front Right
-    VictorSP rearRight = new VictorSP(kRearRightChannel);     // Back Right
-    */
-
     // Invert the right side motors.
     // You may need to change or remove this to match your robot.
     frontLeft.setInverted(true);
-    frontRight.setInverted(true);
+    rearLeft.setInverted(true);
 
     //m_robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
 
@@ -85,132 +64,53 @@ public class omnimech extends TimedRobot {
 
   }
 
-  // Function to normalize inputs between -1 and 1
-  // Thanks to Simone on SE: https://stats.stackexchange.com/a/178629
-  public double normalizeStickVals(double i, double max, double min) {
-    return 2 * (i - min) / (max - min) - 1;
-  }
-
-  public double findMinOrMax(double a, double b, int minOrMax) { // Find the min/max of a value. True = find max, False = find min
-    switch(minOrMax) {
-      case(1):
-          if(a > b) {
-            return a;
-          } else {
-            return b;
-          }
-      case(2):
-          if(a < b) {
-            return a;
-          } else {
-            return b;
-          }
-      default:
-        System.out.println("Please use a valid min/max option. 1 = max, 2 = min");
-        return 99.99;
-    }
-
-  }
-
-  public double getHypo(double a, double b) {
-    // Pythagorean theorem: a^2 + b^2 = c^2
-      // Or: c = sqrt( a^2 + b^2 )
-    return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+  public double realValue(double a) {
+    int fakeBool = (a < 0) ? 1 : 0;   // In C++, this is branchless. Here, we need an if-statement
+    return a + -2 * a * fakeBool;     // If fakeBool is 1, the input value is negative  
   }
 
   @Override
   public void teleopPeriodic() {
-    // Use the joystick X axis for lateral movement, Y axis for forward
-    // movement, and Z axis for rotation.
-    xbox_xAxisRStick = m_stick.getRightX();   // Get stick axies
-    xbox_yAxisRStick = m_stick.getRightY();     //-
-    xbox_xAxisLStick = m_stick.getLeftX();      //-
-    xbox_yAxisLStick = m_stick.getLeftY();      //-
 
-    // Get max and min for each stick
-    xbox_xAxisRStickMAX = findMinOrMax(xbox_xAxisRStickMAX, xbox_xAxisRStick, 1);
-    xbox_xAxisRStickMIN = findMinOrMax(xbox_xAxisRStickMIN, xbox_xAxisRStick, 2);
-    xbox_yAxisRStickMAX = findMinOrMax(xbox_yAxisRStickMAX, xbox_yAxisRStick, 1);
-    xbox_yAxisRStickMIN = findMinOrMax(xbox_yAxisRStickMIN, xbox_yAxisRStick, 2);
+    fakeBool_Forward = (m_stick.getLeftY() >= STICK_DEADZONE || m_stick.getLeftY() <= -STICK_DEADZONE) ? 1 : 0;   // Set the fake boolean to "true" or "false" on whether the stick is outside of the deadband
+    fakeBool_Strafe = (m_stick.getLeftX() >= STICK_DEADZONE || m_stick.getLeftX() <= -STICK_DEADZONE) ? 1 : 0;    // --
+    fakeBool_Rotate = (m_stick.getRightX() >= STICK_DEADZONE || m_stick.getRightX() <= -STICK_DEADZONE) ? 1 : 0;  // --
 
-    xbox_xAxisLStickMAX = findMinOrMax(xbox_xAxisLStickMAX, xbox_xAxisLStick, 1);
-    xbox_xAxisLStickMIN = findMinOrMax(xbox_xAxisLStickMIN, xbox_xAxisLStick, 2);
-    xbox_yAxisLStickMAX = findMinOrMax(xbox_yAxisLStickMAX, xbox_yAxisLStick, 1);
-    xbox_yAxisLStickMIN = findMinOrMax(xbox_yAxisLStickMIN, xbox_yAxisLStick, 2);
-
-    // Weigh values down
-    xbox_xAxisRStickMOD = xbox_xAxisRStick * 0.1;
-    xbox_yAxisRStickMOD = xbox_yAxisRStick * 0.1;
-    xbox_xAxisLStickMOD = xbox_xAxisLStick * 0.1;
-    xbox_yAxisLStickMOD = xbox_yAxisLStick * 0.1;
-    
-    //m_robotDrive.driveCartesian(xbox_xAxisLStickMOD, xbox_yAxisLStickMOD, xbox_xAxisRStickMOD, 0.0);
-    
-    // Normalize stick values between -1 & 1
-    xbox_rStickNormalX = normalizeStickVals(xbox_xAxisRStick, xbox_xAxisRStickMAX, xbox_xAxisRStickMIN);
-    xbox_rStickNormalY = normalizeStickVals(xbox_yAxisRStick, xbox_yAxisRStickMAX, xbox_yAxisRStickMIN);
-    xbox_lStickNormalX = normalizeStickVals(xbox_xAxisLStick, xbox_xAxisLStickMAX, xbox_xAxisLStickMIN);
-    xbox_lStickNormalY = normalizeStickVals(xbox_yAxisLStick, xbox_yAxisLStickMAX, xbox_yAxisLStickMIN);
-
-    // Calculate the angle of the stick
-    double rStick_hypo = getHypo(xbox_rStickNormalX, xbox_rStickNormalY);
-    double rStick_sideA = xbox_rStickNormalX / rStick_hypo;
-    double rStick_sideB = xbox_rStickNormalY / rStick_hypo;
-    xbox_rStickAngle = Math.atan(rStick_sideB / rStick_sideA);
-
-    double lStick_hypo = getHypo(xbox_lStickNormalX, xbox_lStickNormalY);
-    double lStick_sideA = xbox_lStickNormalX / lStick_hypo;
-    double lStick_sideB = xbox_lStickNormalY / lStick_hypo;
-    xbox_lStickAngle = Math.atan(lStick_sideB / lStick_sideA);
-
-    // Set all motors to 50% power
-    frontLeft.set(0.5);
-    frontRight.set(0.5);
-    rearLeft.set(0.5);
-    rearRight.set(0.5);
-
-    // TODO:
-      // Calculate the stick's angle via trig
-      // Use the angle to transforms on motor power
-        // (1, 0) Is strafe right
-        // (-1, 0) Is strafe left
-        // (0, 1) Is drive forward
-        // (0, -1) Is drive backward
-
-        // (sqr2/2, sqr2/2) Is strafe diagonally up-right
-        // (-sqr2/2, sqr2/2) Is strafe diagonally up-left
-        // (-sqr2/2, -sqr2/2) Is strafe diagonally down-left
-        // (sqr2/2, -sqr2/2) Is strafe diagonally down-right
-
-        // There's more but I'm lazy and tired to describe them rn
-
-    if(debugMode == true) {
-      System.out.println("Right Stick X: " + xbox_xAxisRStick);
-      System.out.println("Right Stick Y: " + xbox_yAxisRStick);
-      System.out.println("---");
-      System.out.println("Left Stick X: " + xbox_xAxisLStick);
-      System.out.println("Left Stick Y: " + xbox_yAxisLStick);
-
-      System.out.println("=========");
-
-      System.out.println("Right Stick X (mod): " + xbox_xAxisRStickMOD);
-      System.out.println("Right Stick Y (mod): " + xbox_yAxisRStickMOD);
-      System.out.println("---");
-      System.out.println("Left Stick X (mod): " + xbox_xAxisLStickMOD);
-      System.out.println("Left Stick Y (mod): " + xbox_yAxisLStickMOD);
-
-      System.out.println("|||||||||");
-
-      System.out.println("Right Stick X (normal): " + xbox_rStickNormalX);
-      System.out.println("Right Stick Y (normal): " + xbox_rStickNormalY);
-      System.out.println("---");
-      System.out.println("Left Stick X (normal): " + xbox_lStickNormalX);
-      System.out.println("Left Stick Y (normal): " + xbox_lStickNormalY);
-
-      System.out.println("/////////");
-
-      System.out.println("Right Stick Angle: " + xbox_rStickAngle);
-      System.out.println("Left Stick Angle: " + xbox_lStickAngle);
+    if(fakeBool_Forward == 1) {
+      stickTotal += realValue(m_stick.getLeftY());
     }
+    if(fakeBool_Strafe == 1) {
+      stickTotal += realValue(m_stick.getLeftX());
+    }
+    if(fakeBool_Rotate == 1) {
+      stickTotal += realValue(m_stick.getRightX());
+    }
+
+    leftY_Fine = m_stick.getLeftY() * (realValue(m_stick.getLeftY()) / stickTotal) * fakeBool_Forward;
+    leftX_Fine = m_stick.getLeftX() * (realValue(m_stick.getLeftX()) / stickTotal) * fakeBool_Strafe;
+    rightX_Fine = m_stick.getRightX() * (realValue(m_stick.getRightX()) / stickTotal) * fakeBool_Rotate;
+
+    // Accelleration formula so the motor speed isn't crazy right off the bat
+    accelValues[FORWARD] = (1 / ACCEL_CO) * (m_stick.getLeftY() * -STICK_MOD) + ((ACCEL_CO - 1) / ACCEL_CO);
+    accelValues[STRAFE] = (1 / ACCEL_CO) * (m_stick.getLeftX() * STICK_MOD) + ((ACCEL_CO - 1) / ACCEL_CO);
+    accelValues[ROTATE] = (1 / ACCEL_CO) * (m_stick.getRightX() * STICK_MOD) + ((ACCEL_CO - 1) / ACCEL_CO);
+
+    powVals[FORWARD] = accelValues[FORWARD] * leftY_Fine;
+    powVals[STRAFE] = accelValues[STRAFE] * leftX_Fine;
+    powVals[ROTATE] = accelValues[ROTATE] * rightX_Fine;
+
+    // Idea:
+      // Use weighted averages to set the controller's fine tuning
+        // Find whether a stick axis is greater / less than the deadzone
+        // If so, add its value to the total & flip a boolean
+        // Divide each stick's value by the total, then multiply the value with the previous result
+        // Multiply the stick values by the weight value
+
+    frontRight.set(powVals[FORWARD] - powVals[STRAFE] - powVals[ROTATE]);
+    frontLeft.set(powVals[FORWARD] + powVals[STRAFE] + powVals[ROTATE]);
+    rearRight.set(powVals[FORWARD] + powVals[STRAFE] - powVals[ROTATE]);
+    rearLeft.set(powVals[FORWARD] - powVals[STRAFE] + powVals[ROTATE]);
+
+    stickTotal = 0; // Reset stick total value so it doesn't skyrocket over time
   }
 }
