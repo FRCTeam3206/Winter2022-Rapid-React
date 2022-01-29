6851994b08eec5e0c1b84d8aaf7568482df07e23
CREATE_02 <frc-create@outlook.com>
2022-01-29 14:48:08 -0600

    Working velocity control PID

--- Test_Bench_Motor/src/main/java/frc/robot/Robot.java
+++ Test_Bench_Motor/src/main/java/frc/robot/Robot.java
@@ -4,15 +4,22 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Encoder;
+//import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 //import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+
+import javax.print.attribute.standard.Fidelity;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import org.opencv.core.Mat;
 
 /**
  * This sample program shows how to control a motor using a joystick. In the operator control part
@@ -25,23 +32,60 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
  * to the Dashboard.
  */
 public class Robot extends TimedRobot {
-  private static final int kMotorPort = 0;
+  private static final int kMotorPort = 1;
   private static final int kJoystickPort = 0;
   private static final int kEncoderPortA = 0;
   private static final int kEncoderPortB = 1;
+  private static final double increment = 0.02;
 
-  private MotorController m_motor;
-  private Joystick m_joystick;
-  private Encoder m_encoder;
+  private CANSparkMax m_motor;
+  private XboxController m_joystick;
+  private SparkMaxPIDController m_pidController;
+  private RelativeEncoder m_encoder;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
+  private double setpoint = 0;
+
 
   @Override
   public void robotInit() {
-    m_motor = new PWMSparkMax(kMotorPort);
-    m_joystick = new Joystick(kJoystickPort);
-    m_encoder = new Encoder(kEncoderPortA, kEncoderPortB);
+    m_motor = new CANSparkMax(kMotorPort, MotorType.kBrushless);
+    m_joystick = new XboxController(kJoystickPort);
+    // m_encoder = new Encoder(kEncoderPortA, kEncoderPortB);
+
+    m_motor.restoreFactoryDefaults();
+
+    m_pidController = m_motor.getPIDController();
+
+    m_encoder = m_motor.getEncoder();
+
     // Use SetDistancePerPulse to set the multiplier for GetDistance
     // This is set up assuming a 6 inch wheel with a 360 CPR encoder.
-    m_encoder.setDistancePerPulse((Math.PI * 6) / 360.0);
+    // m_encoder.setDistancePerPulse((Math.PI * 6) / 360.0);
+    kP = 6e-5; 
+    kI = 0;
+    kD = 0; 
+    kIz = 0; 
+    kFF = 0.000015; 
+    kMaxOutput = 1; 
+    kMinOutput = -1;
+    maxRPM = 5700;
+
+    // set PID coefficients
+    m_pidController.setP(kP);
+    m_pidController.setI(kI);
+    m_pidController.setD(kD);
+    m_pidController.setIZone(kIz);
+    m_pidController.setFF(kFF);
+    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+    // display PID coefficients on SmartDashboard
+    SmartDashboard.putNumber("P Gain", kP);
+    SmartDashboard.putNumber("I Gain", kI);
+    SmartDashboard.putNumber("D Gain", kD);
+    SmartDashboard.putNumber("I Zone", kIz);
+    SmartDashboard.putNumber("Feed Forward", kFF);
+    SmartDashboard.putNumber("Max Output", kMaxOutput);
+    SmartDashboard.putNumber("Min Output", kMinOutput);
   }
 
   /*
@@ -50,11 +94,62 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-    SmartDashboard.putNumber("Encoder", m_encoder.getDistance());
+    SmartDashboard.putNumber("Encoder", m_encoder.getVelocity());
   }
 
   @Override
   public void teleopPeriodic() {
-    m_motor.set(m_joystick.getY());
+    // read PID coefficients from SmartDashboard
+    double p = SmartDashboard.getNumber("P Gain", 0);
+    double i = SmartDashboard.getNumber("I Gain", 0);
+    double d = SmartDashboard.getNumber("D Gain", 0);
+    double iz = SmartDashboard.getNumber("I Zone", 0);
+    double ff = SmartDashboard.getNumber("Feed Forward", 0);
+    double max = SmartDashboard.getNumber("Max Output", 0);
+    double min = SmartDashboard.getNumber("Min Output", 0);
+
+    // if PID coefficients on SmartDashboard have changed, write new values to controller
+    if((p != kP)) { m_pidController.setP(p); kP = p; }
+    if((i != kI)) { m_pidController.setI(i); kI = i; }
+    if((d != kD)) { m_pidController.setD(d); kD = d; }
+    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
+    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
+    if((max != kMaxOutput) || (min != kMinOutput)) { 
+      m_pidController.setOutputRange(min, max); 
+      kMinOutput = min; kMaxOutput = max; 
+    }
+
+    /**
+     * PIDController objects are commanded to a set point using the 
+     * SetReference() method.
+     * 
+     * The first parameter is the value of the set point, whose units vary
+     * depending on the control type set in the second parameter.
+     * 
+     * The second parameter is the control type can be set to one of four 
+     * parameters:
+     *  com.revrobotics.CANSparkMax.ControlType.kDutyCycle
+     *  com.revrobotics.CANSparkMax.ControlType.kPosition
+     *  com.revrobotics.CANSparkMax.ControlType.kVelocity
+     *  com.revrobotics.CANSparkMax.ControlType.kVoltage
+     */
+    //double setPoint = -m_joystick.getRightY()*maxRPM;
+    if (m_joystick.getBButtonPressed()) {
+      setpoint = 0.0;
+    } else if (m_joystick.getYButtonPressed()) {
+      setpoint += increment;
+    } else if (m_joystick.getAButtonPressed()) {
+      setpoint -= increment;
+    }
+    setpoint = MathUtil.clamp(setpoint, kMinOutput, kMaxOutput);
+
+    double rpm_target = setpoint*maxRPM;
+
+    m_pidController.setReference(rpm_target, CANSparkMax.ControlType.kVelocity);
+    
+    SmartDashboard.putNumber("SetPoint", setpoint);
+    SmartDashboard.putNumber("RPM_Target", rpm_target);
+    SmartDashboard.putNumber("ProcessVariable", m_encoder.getVelocity());
+    
   }
 }
