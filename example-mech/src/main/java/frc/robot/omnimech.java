1324e855a940c144fb8974d5662fab39d07bf94e
StrangestMan <strangestalt@protonmail.com>
2022-02-01 16:33:12 -0600

    "Fix" stick fine-tuning (not fixed)

--- example-mech/src/main/java/frc/robot/omnimech.java
+++ example-mech/src/main/java/frc/robot/omnimech.java
@@ -6,6 +6,10 @@ package frc.robot;
 
 import java.lang.System;
 
+import org.opencv.features2d.FlannBasedMatcher;
+
+import edu.wpi.first.wpilibj.DriverStation;
+
 //import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
@@ -40,10 +44,10 @@ public class omnimech extends TimedRobot {
   private static int ROTATE = 2;
 
   private static int ACCEL_CO = 8; // Accelleration coefficient
-  private static double STICK_MOD = 1; // Artifical limit on input range
+  private static double STICK_MOD = 0.5; // Artifical limit on input range
   private static double STICK_DEADZONE = 0.1; // xbox controller deadband
 
-  //private double stickTotal = 0;  // Total values of all stick inputs outside the deadband
+  private double stickTotal = 0;  // Total values of all stick inputs outside the deadband
 
   private double stick_LX = 0;
   private double stick_LY = 0;
@@ -53,11 +57,9 @@ public class omnimech extends TimedRobot {
   private int fakeBool_Strafe = 0;
   private int fakeBool_Rotate = 0;
 
-  /*
   private double leftX_Fine = 0;
   private double leftY_Fine = 0;
   private double rightX_Fine = 0;
-  */
 
   private double powVals[] = {0, 0, 0};
 
@@ -109,7 +111,7 @@ public class omnimech extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     if(debugMode == 0) {
-      stick_LX = m_stick.getLeftX() * STICK_MOD * -1;
+      stick_LX = m_stick.getLeftX() * STICK_MOD;
       stick_LY = m_stick.getLeftY() * STICK_MOD * -1;
       stick_RX = m_stick.getRightX() * STICK_MOD;
       
@@ -117,7 +119,6 @@ public class omnimech extends TimedRobot {
       fakeBool_Strafe = (m_stick.getLeftX() >= STICK_DEADZONE || m_stick.getLeftX() <= -1 * STICK_DEADZONE) ? 1 : 0;    // --
       fakeBool_Rotate = (m_stick.getRightX() >= STICK_DEADZONE || m_stick.getRightX() <= -1 * STICK_DEADZONE) ? 1 : 0;  // --
 
-      /*
       if(fakeBool_Forward == 1) {
         stickTotal += realValue(m_stick.getLeftY());
       }
@@ -127,13 +128,21 @@ public class omnimech extends TimedRobot {
       if(fakeBool_Rotate == 1) {
         stickTotal += realValue(m_stick.getRightX());
       }
-      */
 
       /*  This is a good idea, yet a bad implementation
       leftY_Fine = m_stick.getLeftY() * (realValue(m_stick.getLeftY()) / stickTotal) * fakeBool_Forward;
       leftX_Fine = m_stick.getLeftX() * (realValue(m_stick.getLeftX()) / stickTotal) * fakeBool_Strafe;
       rightX_Fine = m_stick.getRightX() * (realValue(m_stick.getRightX()) / stickTotal) * fakeBool_Rotate;
       */
+
+      leftY_Fine = m_stick.getLeftY() * (m_stick.getLeftY() / stickTotal);
+      leftX_Fine = m_stick.getLeftX() * (m_stick.getLeftX() / stickTotal);
+      rightX_Fine = m_stick.getRightX() * (m_stick.getRightX() / stickTotal);
+
+      edu.wpi.first.wpilibj.DriverStation.reportWarning("Left Stick Y Fine: " + leftY_Fine, false);
+      edu.wpi.first.wpilibj.DriverStation.reportWarning("Left Stick X Fine: " + leftX_Fine, false);
+      edu.wpi.first.wpilibj.DriverStation.reportWarning("Right Stick X Fine: " + rightX_Fine, false);
+
 
       // Accelleration formula so the motor speed isn't crazy right off the bat
         // This fomrula is simplified from: 1/k * input + (k - 1)/k * old motor pow = new motor pow
@@ -143,17 +152,17 @@ public class omnimech extends TimedRobot {
         
       // Automatically "break" when the driver isn't pressing anything
       if(fakeBool_Forward == 1) {
-        powVals[FORWARD] = accelValues[FORWARD];// * leftY_Fine;
+        powVals[FORWARD] = accelValues[FORWARD] * leftY_Fine;
       } else {
         powVals[FORWARD] = 0;
       }
       if(fakeBool_Strafe == 1) {
-        powVals[STRAFE] = accelValues[STRAFE];// * leftX_Fine;
+        powVals[STRAFE] = accelValues[STRAFE] * leftX_Fine;
       } else {
         powVals[STRAFE] = 0;
       }
       if(fakeBool_Rotate == 1) {
-        powVals[ROTATE] = accelValues[ROTATE];// * rightX_Fine;
+        powVals[ROTATE] = accelValues[ROTATE] * rightX_Fine;
       } else {
         powVals[ROTATE] = 0;
       }
