48be2090f69f77da0aff7bbcf75c757e2fed7c68
CREATE_02 <frc-create@outlook.com>
2022-01-31 19:24:31 -0600

    Detects ball with ColorMatch Class

--- ColorSensorTest/src/main/java/frc/robot/Robot.java
+++ ColorSensorTest/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.ColorSensorV3.ColorSensorMeasurementRate;
 import com.revrobotics.ColorSensorV3.ColorSensorResolution;
@@ -13,7 +15,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
+import edu.wpi.first.wpilibj.util.Color;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -27,9 +29,14 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
+  private static final Color redBall=new Color(.54,.34,.12);
+  private static final Color blueBall=new Color(.16,.40,.47);
+  private ColorMatch colorMatch=new ColorMatch();
   @Override
   public void robotInit() {
-    colorSensor.configureColorSensor(ColorSensorResolution.kColorSensorRes16bit, ColorSensorMeasurementRate.kColorRate25ms, GainFactor.kGain1x);
+    colorMatch.addColorMatch(redBall);
+    colorMatch.addColorMatch(blueBall);  
+    colorMatch.setConfidenceThreshold(.85);  
   }
 
   /**
@@ -68,9 +75,35 @@ public class Robot extends TimedRobot {
   public void teleopInit() {}
 
   /** This function is called periodically during operator control. */
+  float[] hsv=new float[3];
+  int colorMultiplier=255*2;
   @Override
   public void teleopPeriodic() {
-    System.out.println(colorSensor.getRed());
+    
+    Color detected=colorSensor.getColor();
+    SmartDashboard.putNumber("Red", detected.red);
+    SmartDashboard.putNumber("Green", detected.green);
+    SmartDashboard.putNumber("Blue", detected.blue);
+    hsv=java.awt.Color.RGBtoHSB((int)(colorSensor.getRed()*colorMultiplier),(int)(colorSensor.getGreen()*colorMultiplier),(int)(colorSensor.getBlue()*colorMultiplier),hsv);
+    SmartDashboard.putNumber("Hue",hsv[0]);
+    SmartDashboard.putNumber("Sat",hsv[1]);
+    SmartDashboard.putNumber("Val",hsv[2]);
+    ColorMatchResult matchResult=colorMatch.matchColor(detected);
+    String output="No Ball";
+    double confidence=-1;
+    if(colorMatch.matchColor(detected)!=null&&matchResult.confidence>.75){
+      Color result=matchResult.color;
+      if(result==redBall){
+        output="Red Ball";
+      }else if(result==blueBall){
+        output="Blue Ball";
+      }else{
+        output="No Ball";
+      }
+      confidence=matchResult.confidence;
+    }
+    SmartDashboard.putString("Output", output);
+    SmartDashboard.putNumber("Confidence", confidence);
   }
 
   /** This function is called once when the robot is disabled. */
