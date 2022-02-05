da5fff851c14855234dc747407bb273b5b4851e2
Connor <56924624+IrishConman@users.noreply.github.com>
2022-02-05 13:46:59 -0600

    Test FRC 6624's mechanum code
    https://compendium.readthedocs.io/en/latest/tasks/drivetrains/mecanum.html

--- example-mech/src/main/java/frc/robot/omnimech.java
+++ example-mech/src/main/java/frc/robot/omnimech.java
@@ -34,7 +34,7 @@ public class omnimech extends TimedRobot {
   private VictorSP frontRight = new VictorSP(kFrontRightChannel);  // Front Right
   private VictorSP rearRight = new VictorSP(kRearRightChannel);     // Back Right
 
-  private static int debugMode = 0; // 0 for normal operation. Other numbers result in other functionality
+  private static int debugMode = 3; // 0 for normal operation. Other numbers result in other functionality
 
   private double accelValues[] = {0, 0, 0};
   private static int FORWARD = 0;
@@ -42,7 +42,7 @@ public class omnimech extends TimedRobot {
   private static int ROTATE = 2;
 
   private static int ACCEL_CO = 8; // Accelleration coefficient
-  private static double STICK_MOD = 0.5; // Artifical limit on input range
+  private double STICK_MOD = 0.5; // Artifical limit on input range
   private static double STICK_DEADZONE = 0.1; // xbox controller deadband
 
   private double stickTotal = 0;  // Total values of all stick inputs outside the deadband
@@ -56,6 +56,9 @@ public class omnimech extends TimedRobot {
   private double rightX_Fine = 0;
 
   private double powVals[] = {0, 0, 0};
+
+  private boolean aPressed = false;
+  private boolean yPressed = false;
 
   @Override
   public void robotInit() {
@@ -96,8 +99,14 @@ public class omnimech extends TimedRobot {
         rearRight.set(leftStick_Y + leftStick_X - rightStickX);
         rearLeft.set(leftStick_Y - leftStick_X + rightStickX);
       break;
+      case(3): // Test FRC 6624's mecanum code
+        mechDrive6624();
+      break;
       default:
-        System.out.println("[DEBUG] Error: Incorrect debugMode value");
+        String msg = "[DEBUG] Error: Incorrect debugMode value";
+        System.out.println(msg);
+        reportWarning(msg);
+        endCompetition();
       return;
     }
   }
@@ -142,7 +151,8 @@ public class omnimech extends TimedRobot {
       */
     
     if(isDirection == true) {
-      return accel * fine;
+      //return accel * fine;
+      return fine * STICK_MOD;
     } else {
       return 0;
     }
@@ -185,6 +195,74 @@ public class omnimech extends TimedRobot {
     }
   }
 
+  protected void incSpeed() {
+    if(m_stick.getAButton() && aPressed != true) {
+      if(STICK_MOD + 0.1 <= 1) {
+        STICK_MOD += 0.1;
+      }
+        aPressed = true;
+    }
+    if(m_stick.getAButton() == false) {
+      aPressed = false;
+    }
+
+  }
+
+  protected void decSpeed() {
+    if(m_stick.getYButton() && yPressed != true) {
+      if(STICK_MOD - 0.1 >= 0.4) {
+        STICK_MOD -= 0.1;
+      }
+      yPressed = true;
+    }
+    if(m_stick.getYButton() == false) {
+      yPressed = false;
+    }
+  }
+
+  protected double calcAngle(double x, double y) {
+    double r = Math.sqrt((Math.pow(x, 2) + Math.pow(y, 2)));
+    return Math.acos(x / r);
+  }
+
+  protected double calcTransPower(double x, double y) {
+    int isX = (checkDeadzone(x)) ? 1 : 0;
+    int isY = (checkDeadzone(y)) ? 1 : 0;
+
+    int stickTotal = isX + isY;
+    double sticks = x * isX + y * isY;
+
+    return stickFine(sticks, stickTotal * -1, stickTotal, -1, 1);
+  }
+
+  protected void experimentalDrive(double translationAngle, double translationPower, double turnPower) {
+    double ADPower = translationPower * Math.sqrt(2) * 0.5 * (Math.sin(translationAngle) + Math.cos(translationAngle));
+    double BCPower = translationPower * Math.sqrt(2) * 0.5 * (Math.sin(translationAngle) - Math.cos(translationAngle));
+  
+    double turningScale = Math.max(Math.abs(ADPower + turnPower), Math.abs(ADPower - turnPower));
+    turningScale = Math.max(turningScale, Math.max(Math.abs(BCPower + turnPower), Math.abs(BCPower - turnPower)));
+
+    if(Math.abs(turningScale) > 1) {
+      turningScale = 1;
+    }
+
+    frontLeft.set(ADPower);
+    rearLeft.set(BCPower);
+    frontRight.set(BCPower);
+    rearRight.set(ADPower);
+  }
+
+  protected void mechDrive6624() {
+    double x = m_stick.getLeftX();
+    double y = m_stick.getLeftY();
+    double z = m_stick.getRightX();
+
+    double angle = calcAngle(x, y);
+    double transPower = calcTransPower(x, y);
+
+    experimentalDrive(angle, transPower, z);
+  }
+
   protected void mechDriveCRW() {
     // Idea:
         // Use weighted averages to set the controller's fine tuning
@@ -194,7 +272,7 @@ public class omnimech extends TimedRobot {
           // Multiply the stick values by the weight value
     
     stick_LX = m_stick.getLeftX() * STICK_MOD;
-    stick_LY = m_stick.getLeftY() * STICK_MOD * -1;
+    stick_LY = m_stick.getLeftY() * STICK_MOD;
     stick_RX = m_stick.getRightX() * STICK_MOD;
           
     boolean isForward = checkDeadzone(m_stick.getLeftY());
@@ -214,19 +292,23 @@ public class omnimech extends TimedRobot {
     accelValues[STRAFE] = setAccelVals(stick_LX, powVals[STRAFE]);
     accelValues[ROTATE] = setAccelVals(stick_RX, powVals[ROTATE]);
     
-    powVals[FORWARD] = setPowVals(accelValues[FORWARD], leftY_Fine, isForward);
+    powVals[FORWARD] = setPowVals(accelValues[FORWARD], leftY_Fine, isForward) * -1;
     powVals[STRAFE] = setPowVals(accelValues[STRAFE], leftX_Fine, isStrafe);
     powVals[ROTATE] = setPowVals(accelValues[ROTATE], rightX_Fine, isRotate);
           
     reportWarning("Forward Power: " + powVals[FORWARD]);
     reportWarning("Strafe Power: " + powVals[STRAFE]);
     reportWarning("Rotate Power: " + powVals[ROTATE]);
+    reportWarning("Speed mod: " + STICK_MOD);
     
     frontRight.set(setMechanumSpeed(powVals[FORWARD], powVals[STRAFE], powVals[ROTATE], 0));
     frontLeft.set(setMechanumSpeed(powVals[FORWARD], powVals[STRAFE], powVals[ROTATE], 1));
     rearRight.set(setMechanumSpeed(powVals[FORWARD], powVals[STRAFE], powVals[ROTATE], 2));
     rearLeft.set(setMechanumSpeed(powVals[FORWARD], powVals[STRAFE], powVals[ROTATE], 3));
     
+    incSpeed();
+    decSpeed();
+
     stickTotal = 0; // Reset stick total value so it doesn't skyrocket over time
   }
 
