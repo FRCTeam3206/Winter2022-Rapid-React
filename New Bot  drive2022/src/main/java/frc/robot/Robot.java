99f591682c18b7ce8b608b9af2ab60a4a383976a
Ollie Stepan <ojstepan@gmail.com>
2022-02-08 19:58:57 -0600

    Basic teleop code
    code examples taken from Chewbreaka code to fit this years needs

--- New Bot  drive2022/src/main/java/frc/robot/Robot.java
+++ New Bot  drive2022/src/main/java/frc/robot/Robot.java
@@ -8,23 +8,105 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import java.lang.Math;
+import edu.wpi.first.wpilibj.Timer;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+
+import edu.wpi.first.wpilibj.util.Color;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.cscore.UsbCamera;
+
+//import edu.wpi.first.cameraserver.CameraServer;
+//import edu.wpi.first.wpilibj.AddressableLED;
+//import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
  * arcade steering.
  */
 public class Robot extends TimedRobot {
+
+  SendableChooser<String> autoChoices = new SendableChooser<>();
+  String autoSelected;
+
+  //the folliwing four lines are part of the original basic code
+ 
   private final PWMSparkMax m_leftMotor = new PWMSparkMax(0);
   private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
   private final Joystick m_stick = new Joystick(0);
+
+
+  // DriveTrain
+  DifferentialDrive chewbreakaDrive;
+  // Drivetrain Motors
+  CANSparkMax leftFrontDrive;
+  CANSparkMax rightFrontDrive;
+  CANSparkMax leftBackDrive;
+  CANSparkMax rightBackDrive;
+
+  String DesiredDirection;
+  double desiredDistance;
+  double distanceTraveled;
+  double tLateDrive = 18;//longest time it takes to drive for any function
+  double tLateTurn = 15;//longest time it takes to drive for any function
+
+  Timer velocityTimer = new Timer();
+
+  // DriveTrain Pneumatics
+  DoubleSolenoid driveSol = new DoubleSolenoid(0, PneumaticsModuleType.CTREPCM, 0, 1);
+  PowerDistribution pdp = new PowerDistribution(0, ModuleType.kCTRE);
+  Compressor compressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
+  // DriveTrain Encoders
+  RelativeEncoder leftEncoder;
+  RelativeEncoder rightEncoder;
 
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
+
+    leftFrontDrive = new CANSparkMax(1, MotorType.kBrushless);
+    rightFrontDrive = new CANSparkMax(3, MotorType.kBrushless);
+    leftBackDrive = new CANSparkMax(2, MotorType.kBrushless);
+    rightBackDrive = new CANSparkMax(4, MotorType.kBrushless);
+    leftEncoder = leftFrontDrive.getEncoder();
+    rightEncoder = rightFrontDrive.getEncoder();
+
+    leftFrontDrive.restoreFactoryDefaults();
+    rightFrontDrive.restoreFactoryDefaults();
+    leftBackDrive.restoreFactoryDefaults();
+    rightBackDrive.restoreFactoryDefaults();
+    leftEncoder.setPosition(0);
+    rightEncoder.setPosition(0);
+
+    leftBackDrive.follow(leftFrontDrive);
+    rightBackDrive.follow(rightFrontDrive);
+
+    chewbreakaDrive = new DifferentialDrive(leftFrontDrive, rightFrontDrive);
+   
+    //m_rightMotor is part of original code
     m_rightMotor.setInverted(true);
+    
   }
 
   @Override
@@ -34,4 +116,62 @@ public class Robot extends TimedRobot {
     // and backward, and the X turns left and right.
     m_robotDrive.arcadeDrive(-m_stick.getY(), m_stick.getX());
   }
-}
+
+  public void Drive(double distance) {
+    distanceTraveled = leftEncoder.getPosition() * -1 / 12;
+    desiredDistance = distance + distanceTraveled;
+    velocityTimer.start();
+    DriveLabel: if (distance > 0) {
+      while (desiredDistance > distanceTraveled) {
+        distanceTraveled = leftEncoder.getPosition() * -1 / 12;
+        chewbreakaDrive.tankDrive(-.6, -.6);
+        if (velocityTimer.get() >= tLateDrive) {
+          break DriveLabel;
+        }
+      }
+    } else if (distance < 0) {
+      while (desiredDistance < distanceTraveled) {
+        distanceTraveled = leftEncoder.getPosition() * -1 / 12;
+        chewbreakaDrive.tankDrive(.7, .7);
+        if (velocityTimer.get() >= tLateDrive) {
+          break DriveLabel;
+        }
+      }
+    } else {
+      chewbreakaDrive.tankDrive(0, 0);
+    }
+    chewbreakaDrive.tankDrive(0, 0);
+  }
+
+  @Override
+  public void autonomousInit() {
+    autoSelected = autoChoices.getSelected();
+
+    compressor.enableDigital();
+    driveSol.set(Value.kReverse);
+    switch (autoSelected) {
+    case "AutoLine":
+      Drive(5);
+      break;
+      /*
+    case "Center Shot":
+      CenterShot();
+      break;
+    case "Left Shot":
+      LeftShot();
+      break;
+    case "Right Shot":
+      RightShot();
+      break;
+    case "4Auto":
+      // 4Auto();
+      break;
+    default:
+      Drive(5);
+      break;
+      */
+    }
+  }
+  }
+
+
