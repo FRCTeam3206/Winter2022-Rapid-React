15dcfaeb08ad0d83437f1129cdb9c039924d3fc8
CREATE_02 <frc-create@outlook.com>
2022-02-18 15:22:17 -0600

    Implemented a Basic intake class- no pnenumatics

--- ChronosTankDrive/src/main/java/frc/robot/subsystems/Intake.java
+++ ChronosTankDrive/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,18 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.XboxController;
+
 public class Intake {
-    
+    XboxController joystick;
+    VictorSPX intakeMotor;
+    public Intake(int motorId,XboxController joystick){
+        this.joystick=joystick;
+        intakeMotor=new VictorSPX(motorId);
+    }
+    public void intakePeriodic(){
+        intakeMotor.set(VictorSPXControlMode.PercentOutput,joystick.getRightTriggerAxis());
+    }
 }
