3a264faf2325a774a725fdf6cf0f8efe7f4be33d
CREATE_02 <frc-create@outlook.com>
2022-02-19 10:22:41 -0600

    Reverted to using single solnoid object for intake

--- ChronosTankDrive/src/main/java/frc/robot/subsystems/Intake.java
+++ ChronosTankDrive/src/main/java/frc/robot/subsystems/Intake.java
@@ -5,26 +5,25 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 public class Intake extends Subsystem{
     XboxController joystick;
     VictorSPX intakeMotor;
-    Solenoid rightDeploy,leftDeploy;
+    DoubleSolenoid deploy;
     public Intake(int motorId,int pistonId1,int pistonId2,XboxController joystick){
         this.joystick=joystick;
         intakeMotor=new VictorSPX(motorId);
-        rightDeploy=new Solenoid(PneumaticsModuleType.CTREPCM,pistonId1);
-        leftDeploy=new Solenoid(PneumaticsModuleType.CTREPCM,pistonId2);
+        deploy=new DoubleSolenoid(PneumaticsModuleType.CTREPCM,pistonId1,pistonId2);
     }
     boolean deployed=false;
     public void periodic(){
         intakeMotor.set(VictorSPXControlMode.PercentOutput,joystick.getRightTriggerAxis());
-        if(joystick.getRightBumperPressed()){
-            rightDeploy.toggle();
-            leftDeploy.toggle();
+        if(joystick.getRightBumper()!=deployed){
+            deployed=joystick.getRightBumper();
+            if(deployed) deploy.set(Value.kForward);
+            else deploy.set(Value.kReverse);
         }
     }
     @Override
