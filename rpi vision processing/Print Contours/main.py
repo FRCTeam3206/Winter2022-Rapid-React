3fc88ecf3096dae6df70b14aaa4121360ce9f39c
CREATE_01 <frc-create@outlook.com>
2022-02-28 20:06:24 -0600

    Imported limelight code started contour detection

--- rpi vision processing/Print Contours/main.py
+++ rpi vision processing/Print Contours/main.py
@@ -7,7 +7,8 @@ import numpy as np
 
 w = 160
 h = 120
-
+def drawRectangle(image,rect,color):
+    cv2.rectangle(image,(int(rect[0]),int(rect[1])),(int(rect[0]+rect[2]),int(rect[1]+rect[3])),color,2)
 def main():
     cs = CameraServer.getInstance()
     cs.enableLogging()
@@ -23,21 +24,26 @@ def main():
     outputStream = cs.putVideo("Name", w, h)
 
     # Allocating new images is very expensive, always try to preallocate
-    img = np.zeros(shape=(w, h, 3), dtype=np.uint8)
+    image = np.zeros(shape=(w, h, 3), dtype=np.uint8)
 
     while True:
         # Tell the CvSink to grab a frame from the camera and put it
         # in the source image.  If there is an error notify the output.
-        time, img = cvSink.grabFrame(img)
+        time, image = cvSink.grabFrame(image)
         if time == 0:
             # Send the output the error.
             outputStream.notifyError(cvSink.getError())
             # skip the rest of the current iteration
             continue
-
-        #
-        # Insert your image processing logic here!
-        #
-
-        # (optional) send some image back to the dashboard
-        outputStream.putFrame(img)
+    img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    #red
+    img_threshold = cv2.inRange(img_hsv, (88, 70, 118), (100, 144, 255))
+   #blue
+   #img_threshold = cv2.inRange(img_hsv, (161, 56, 203), (172, 156, 255))
+    contours, _ = cv2.findContours(img_threshold, 
+    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    if len(contours)>0:
+        drawRectangle(img,cv2.boundingRect(contours[0]),(255,255,255))
+        print(cv2.boundingRect(contours[0]))
+    outputStream.putFrame(img)
