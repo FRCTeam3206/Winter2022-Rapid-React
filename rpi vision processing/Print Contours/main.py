b965077e697291b8bdb1a46669106d9790497d3d
kgobran <34401245+kgobran@users.noreply.github.com>
2022-03-01 16:27:30 -0600

    puts a box around the top 5 options. Added SmartDashboard tuning.

--- rpi vision processing/Print Contours/main.py
+++ rpi vision processing/Print Contours/main.py
@@ -8,6 +8,18 @@ import numpy as np
 # Import networktables
 from networktables import NetworkTables
 
+NetworkTables.initialize(server="10.32.6.2")
+locator_table = NetworkTables.getTable("BallLocator")
+
+lowh = locator_table.getEntry("lowh")
+lows = locator_table.getEntry("lows")
+lowv = locator_table.getEntry("lowv")
+
+highh = locator_table.getEntry("highh")
+highs = locator_table.getEntry("highs")
+highv = locator_table.getEntry("highv")
+
+
 w = 160
 h = 120
 def drawRectangle(image,rect,color):
@@ -29,6 +41,7 @@ def main():
 
     # (optional) Setup a CvSource. This will send images back to the Dashboard
     outputStream = cs.putVideo("Name", 160, 120)
+    thresholdStream = cs.putVideo("Thresholded image", 160, 120)
 
     # Allocating new images is very expensive, always try to preallocate
     image = np.zeros(shape=(w, h, 3), dtype=np.uint8)
@@ -44,13 +57,16 @@ def main():
             continue
         img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         #red
-        img_threshold = cv2.inRange(img_hsv, (88, 70, 118), (100, 144, 255))
+        img_threshold = cv2.inRange(img_hsv, 
+            (lowh.getNumber(0), lows.getNumber(0), lowv.getNumber(0)), 
+            (highh.getNumber(0), highs.getNumber(0), highv.getNumber(0))
+        )
         #blue
         #img_threshold = cv2.inRange(img_hsv, (161, 56, 203), (172, 156, 255))
-        contours, _ = cv2.findContours(img_threshold, 
-        cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        _, contours, _ = cv2.findContours(img_threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours = sorted(contours, key=cv2.contourArea, reverse=True)
-        if len(contours)>0:
-            drawRectangle(img,cv2.boundingRect(contours[0]),(255,255,255))
-            print(cv2.boundingRect(contours[0]))
-        outputStream.putFrame(img)
+        for contour in contours[:5]:
+            drawRectangle(image, cv2.boundingRect(contour),(255,255,255))
+
+        outputStream.putFrame(image)
+        thresholdStream.putFrame(img_threshold)
