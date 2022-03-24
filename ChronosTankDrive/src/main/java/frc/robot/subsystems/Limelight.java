5f8f6216e59d5cc539e0d58056438ed78fc63329
CREATE_02 <frc-create@outlook.com>
2022-03-24 12:33:41 -0500

    sped up climber

--- ChronosTankDrive/src/main/java/frc/robot/subsystems/Limelight.java
+++ ChronosTankDrive/src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
