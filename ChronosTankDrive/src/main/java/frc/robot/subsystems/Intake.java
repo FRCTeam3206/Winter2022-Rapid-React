858ad9714e11f18a10a24a65a2791a7d5f927b4e
CREATE_01 <frc-create@outlook.com>
2022-04-12 16:34:26 -0500

    Self explainitory changes

--- ChronosTankDrive/src/main/java/frc/robot/subsystems/Intake.java
+++ ChronosTankDrive/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,10 +1,14 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
+import frc.robot.Constants;
+import frc.robot.subsystems.shooter.ShooterSupersystem;
+
 import static frc.robot.Constants.Buttons.*;
 
 public class Intake extends Subsystem {
@@ -12,7 +16,7 @@ public class Intake extends Subsystem {
     VictorSPX intakeMotor;
     Solenoid deploy;
     boolean on = false;
-
+    private VictorSPX agitator;
     public Solenoid getDeploy() {
         return deploy;
     }
@@ -26,10 +30,10 @@ public class Intake extends Subsystem {
         intakeMotor = new VictorSPX(motorId);
         deploy = new Solenoid(PneumaticsModuleType.CTREPCM, pistonId1);
         deploy.set(on);
+
     }
 
     boolean deployed = false;
-
     public void periodic() {
         /*
          * if(joystick.getRawButton(B_INTAKE)){
@@ -49,6 +53,7 @@ public class Intake extends Subsystem {
             intakeMotor.set(ControlMode.PercentOutput, 0.7);
         } else {
             intakeMotor.set(ControlMode.PercentOutput, 0);
+            ShooterSupersystem.stopAgitate();
         }
     }
 
