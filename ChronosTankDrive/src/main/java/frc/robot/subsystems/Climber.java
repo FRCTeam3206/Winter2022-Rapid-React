0ef3ad595e24f988570c652fadd2eb909b671d19
CREATE_01 <frc-create@outlook.com>
2022-04-21 17:07:33 -0500

    Switched to double solinoid Untested

--- ChronosTankDrive/src/main/java/frc/robot/subsystems/Climber.java
+++ ChronosTankDrive/src/main/java/frc/robot/subsystems/Climber.java
@@ -3,22 +3,24 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 public class Climber extends Subsystem {
     VictorSPX motor1, motor2;
     GenericHID joystick;
-    Solenoid sol;
+    DoubleSolenoid sol;
 
-    public Climber(int port1, int port2, int solPort, GenericHID joystick) {
+    public Climber(int port1, int port2, int solPort1,int solPort2, GenericHID joystick) {
         this.motor1 = new VictorSPX(port1);
         this.motor2 = new VictorSPX(port2);
         motor2.follow(motor1);
         this.joystick = joystick;
-        sol = new Solenoid(PneumaticsModuleType.CTREPCM, solPort);
+        sol = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, solPort1,solPort2);
     }
 
     @Override
@@ -31,15 +33,13 @@ public class Climber extends Subsystem {
     public void periodic() {
         if (joystick.getPOV() == 0) {
             motor1.set(ControlMode.PercentOutput, 1);
+            sol.set(Value.kReverse);
         } else if (joystick.getPOV() == 180) {
             motor1.set(ControlMode.PercentOutput, -1);
+            sol.set(Value.kForward);
         } else {
             motor1.set(ControlMode.PercentOutput, 0);
-        }
-        if (joystick.getPOV() == 90) {
-            sol.set(true);
-        } else if (joystick.getPOV() == 270) {
-            sol.set(false);
+            sol.set(Value.kReverse);
         }
     }
 
