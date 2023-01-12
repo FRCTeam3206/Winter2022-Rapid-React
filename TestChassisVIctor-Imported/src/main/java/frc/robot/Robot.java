f7a2d77425b3a7bc4c61918aa1ec1b31af454bd0
CREATE-02 <97846880+RoyalT-Wrecks@users.noreply.github.com>
2023-01-12 15:43:48 -0600

    first commit

---
+++ TestChassisVIctor-Imported/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+
+/**
+ * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
+ * arcade steering.
+ */
+public class Robot extends TimedRobot {
+  private final VictorSPX m_leftMotor = new VictorSPX(2);
+  private final VictorSPX m_rightMotor = new VictorSPX(1);
+  //private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+  private final Joystick m_stick = new Joystick(0);
+
+  @Override
+  public void robotInit() {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    m_leftMotor.setInverted(true);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // Drive with arcade drive.
+    // That means that the Y axis drives forward
+    // and backward, and the X turns left and right.
+    /*double yaw = SmartDashboard.getNumber("photonvision/USB_CAMERA/targetYaw", 0);
+    if (m_stick.getRawButton(1) && Math.abs(yaw) < 5) {
+        double adj = yaw * 0.5;
+        m_rightMotor.set(ControlMode.Velocity, adj);
+        m_leftMotor.set(ControlMode.Velocity, -adj);
+    } else {*/
+      m_rightMotor.set(ControlMode.PercentOutput,-m_stick.getRawAxis(1)-m_stick.getRawAxis(0));
+      m_leftMotor.set(ControlMode.PercentOutput,-m_stick.getRawAxis(1)+m_stick.getRawAxis(0));
+    //}
+  }
+}
